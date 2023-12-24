af16ad856d5209da7cb74e528c30f000888469da
Jayendra Naik <jayendranaik007@gmail.com>
2023-12-24 23:56:56 +0530

    picture-in-picture-done

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
-console.log("Testing...");
+const videoElement = document.getElementById("video");
+const button = document.getElementById("button");
+
+// Prompt to select media stream, pass to video element, then play
+async function selectMediaStream() {
+  try {
+    const mediaStream = await navigator.mediaDevices.getDisplayMedia();
+    videoElement.srcObject = mediaStream;
+    videoElement.onloadedmetadata = () => {
+      videoElement.play();
+    };
+  } catch (err) {
+    console.log("Err :", err);
+  }
+}
+
+// When clicked on START button
+button.addEventListener("click", async () => {
+  // Disable Button
+  button.disable = true;
+  // Start picture in picture
+  await videoElement.requestPictureInPicture();
+  // Reset Button
+  button.disable = false;
+});
+
+// On load/reload
+selectMediaStream();
